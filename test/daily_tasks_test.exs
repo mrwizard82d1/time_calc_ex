2ed4d57c73f965cb96a25f932331cc9a5e8831e6
Larry Jones <larry.jones@reveal-energy.com>
2021-04-09 14:28:13 -0500

    TimeCalc.DailyTasks.parse_date with invalid dates

--- test/daily_tasks_test.exs
+++ test/daily_tasks_test.exs
@@ -2,7 +2,14 @@ defmodule DailyTasksTest do
   use ExUnit.Case
 
   test "make_date when date valid" do
-    assert TimeCalc.DailyTasks.make_date({"h1", [], ["05-Apr"], nil}) ==
+    assert TimeCalc.DailyTasks.make_date({"h1", [], ["05-Apr"], %{}}) ==
              NaiveDateTime.new!(NaiveDateTime.local_now().year, 4, 5, 0, 0, 0)
   end
+
+  test "make_date fails when date invalid" do
+    assert_raise MatchError, fn() -> TimeCalc.DailyTasks.make_date({"h1", [], ["17-Nob"], %{}}) end
+    assert_raise MatchError, fn() -> TimeCalc.DailyTasks.make_date({"h1", [], ["31-Jun"], %{}}) end
+    assert_raise MatchError, fn() -> TimeCalc.DailyTasks.make_date({"h1", [], ["3-Mar"], %{}}) end
+  end
+
 end
