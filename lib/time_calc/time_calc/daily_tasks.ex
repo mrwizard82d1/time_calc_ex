3b8f16a147eb8249425d97bcf71348a0b885a32e
Larry Jones <larry.jones@reveal-energy.com>
2021-04-09 11:17:42 -0500

    Begin parsing text and date using Timex

--- lib/time_calc/time_calc/daily_tasks.ex
+++ lib/time_calc/time_calc/daily_tasks.ex
@@ -3,6 +3,19 @@ defmodule TimeCalc.DailyTasks do
   Module for managing activities.
   """
 
-  def make(activity), do: IO.inspect(activity)
+  def make_date({"h1", [], [date_text], _}) do
+    {:ok, partial_date} = Timex.parse(date_text, "{0D}-{Mshort}")
+    %NaiveDateTime{partial_date | year: NaiveDateTime.local_now().year}
+  end
+
+  def make_tasks(date, {"p", [], [tasks_text], _}) do
+    IO.inspect date
+    IO.inspect tasks_text
+  end
+
+  def make([date_ast, task_asts]) do
+    date = make_date(date_ast)
+    tasks = make_tasks(date, task_asts)
+  end
 
 end
