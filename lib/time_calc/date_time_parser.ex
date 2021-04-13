9da43d76273592fbaf196546d2c2b30191c9bd52
Larry Jones <larry.jones@reveal-energy.com>
2021-04-13 10:31:34 -0500

    Change parse_time_text to return ParsedTime

--- lib/time_calc/date_time_parser.ex
+++ lib/time_calc/date_time_parser.ex
@@ -30,7 +30,7 @@ defmodule TimeCalc.DateTimeParser do
         end
         case month do
           {:error, reason} -> {:error, reason}
-          _ -> NaiveDateTime.new(NaiveDateTime.local_now().year, month, day_of_month, 0, 0, 0)
+          _ -> Date.new(NaiveDateTime.local_now().year, month, day_of_month)
         end
       true -> {:error, "Date must have exactly 6 characters but was '#{date_text}'"}
     end
@@ -44,17 +44,18 @@ defmodule TimeCalc.DateTimeParser do
         candidate_hour = String.to_integer(hour_text)
         cond do
           0 <= candidate_hour and candidate_hour <= 24 ->
-            hour = rem(candidate_hour, 24)
-            day_of_month = cond do
-              candidate_hour == 24 -> 2
-              true -> 1
+            is_midnight = if candidate_hour == 24 do
+              true
+            else
+              false
             end
+            hour = rem(candidate_hour, 24)
             minute_text = String.slice(time_text, 2..3)
             candidate_minute = String.to_integer(minute_text)
             cond do
               0 <= candidate_minute and candidate_minute <= 59 ->
                 minute = candidate_minute
-                NaiveDateTime.new(0, 1, day_of_month, hour, minute, 0)
+                %TimeCalc.DateTimeParser.ParsedTime{time: Time.new!(hour, minute, 0), is_midnight: is_midnight}
               true -> {:error, "Minute must be between 0 and 59, inclusive, but was '#{minute_text}'"}
             end
           true -> {:error, "Hour must be between 0 and 24, inclusive, but was '#{hour_text}'"}
--- test/date_time_parser_test.exs
+++ test/date_time_parser_test.exs
@@ -3,27 +3,31 @@ defmodule DateTimeParserTest do
 
   test "parse date text when date valid" do
     year = NaiveDateTime.local_now().year
-    assert TimeCalc.DateTimeParser.parse_date_text("05-Apr") == {:ok, NaiveDateTime.new!(year, 4, 5, 0, 0, 0)}
-    assert TimeCalc.DateTimeParser.parse_date_text("01-Aug") == {:ok, NaiveDateTime.new!(year, 8, 1, 0, 0, 0)}
-    assert TimeCalc.DateTimeParser.parse_date_text("01-Dec") == {:ok, NaiveDateTime.new!(year, 12, 1, 0, 0, 0)}
+    assert TimeCalc.DateTimeParser.parse_date_text("05-Apr") == {:ok, Date.new!(year, 4, 5)}
+    assert TimeCalc.DateTimeParser.parse_date_text("01-Aug") == {:ok, Date.new!(year, 8, 1)}
+    assert TimeCalc.DateTimeParser.parse_date_text("01-Dec") == {:ok, Date.new!(year, 12, 1)}
 
-    assert TimeCalc.DateTimeParser.parse_date_text("28-Feb") == {:ok, NaiveDateTime.new!(year, 2, 28, 0, 0, 0)}
-    assert TimeCalc.DateTimeParser.parse_date_text("31-Jul") == {:ok, NaiveDateTime.new!(year, 7, 31, 0, 0, 0)}
-    assert TimeCalc.DateTimeParser.parse_date_text("31-Dec") == {:ok, NaiveDateTime.new!(year, 12, 31, 0, 0, 0)}
+    assert TimeCalc.DateTimeParser.parse_date_text("28-Feb") == {:ok, Date.new!(year, 2, 28)}
+    assert TimeCalc.DateTimeParser.parse_date_text("31-Jul") == {:ok, Date.new!(year, 7, 31)}
+    assert TimeCalc.DateTimeParser.parse_date_text("31-Dec") == {:ok, Date.new!(year, 12, 31)}
   end
 
   test "parse date text when date invalid" do
     year = NaiveDateTime.local_now().year
     assert TimeCalc.DateTimeParser.parse_date_text("05-Mat") == {:error, "Unrecognized short month, 'Mat'."}
-    assert TimeCalc.DateTimeParser.parse_date_text("00-Aug") == NaiveDateTime.new(year, 8, 0, 0, 0, 0)
-    assert TimeCalc.DateTimeParser.parse_date_text("31-Mar") == NaiveDateTime.new(year, 3, 31, 0, 0, 0)
+    assert TimeCalc.DateTimeParser.parse_date_text("00-Aug") == Date.new(year, 8, 0)
+    assert TimeCalc.DateTimeParser.parse_date_text("31-Mar") == Date.new(year, 3, 31)
   end
 
   test "parse time text when time valid" do
-    assert TimeCalc.DateTimeParser.parse_time_text("0000") == {:ok, NaiveDateTime.new!(0, 1, 1, 0, 0, 0)}
-    assert TimeCalc.DateTimeParser.parse_time_text("2217") == {:ok, NaiveDateTime.new!(0, 1, 1, 22, 17, 0)}
-    assert TimeCalc.DateTimeParser.parse_time_text("0859") == {:ok, NaiveDateTime.new!(0, 1, 1, 8, 59, 0)}
-    assert TimeCalc.DateTimeParser.parse_time_text("2400") == {:ok, NaiveDateTime.new!(0, 1, 2, 0, 0, 0)}
+    assert {:ok, TimeCalc.DateTimeParser.parse_time_text("0000")} ==
+             {:ok, %TimeCalc.DateTimeParser.ParsedTime{time: Time.new!(0, 0, 0)}}
+    assert {:ok, TimeCalc.DateTimeParser.parse_time_text("2217")} ==
+             {:ok, %TimeCalc.DateTimeParser.ParsedTime{time: Time.new!(22, 17, 0)}}
+    assert {:ok, TimeCalc.DateTimeParser.parse_time_text("0859")} ==
+             {:ok, %TimeCalc.DateTimeParser.ParsedTime{time: Time.new!(8, 59, 0)}}
+    assert {:ok, TimeCalc.DateTimeParser.parse_time_text("2400") }==
+             {:ok, %TimeCalc.DateTimeParser.ParsedTime{time: Time.new!(0, 0, 0), is_midnight: true}}
   end
 
   test "parse time text when time invalid" do
